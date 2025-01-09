00496d01e809edd0742c029312c27a1faa6e57da
Jeongho Nam <samchon.github@gmail.com>
2025-01-09 20:44:12 +0900

    Expand `ISwaggerSchema` to LLM schemas.

--- src/ISwaggerSchema.ts
+++ src/ISwaggerSchema.ts
@@ -17,4 +17,58 @@ declare module "@samchon/openapi" {
       export interface __IAttribute extends ISwaggerSchemaCommonPlugin {}
     }
   }
+
+  export namespace IChatGptSchema {
+    export interface IInteger extends ISwaggerSchemaPaymentPlugin.INumeric {}
+    export interface INumber extends ISwaggerSchemaPaymentPlugin.INumeric {}
+    export interface IString
+      extends ISwaggerSchemaPaymentPlugin.IString,
+        ISwaggerSchemaSecurityPlugin {}
+    export interface __IAttribute extends ISwaggerSchemaCommonPlugin {}
+  }
+
+  export namespace IClaudeSchema {
+    export interface IInteger extends ISwaggerSchemaPaymentPlugin.INumeric {}
+    export interface INumber extends ISwaggerSchemaPaymentPlugin.INumeric {}
+    export interface IString
+      extends ISwaggerSchemaPaymentPlugin.IString,
+        ISwaggerSchemaSecurityPlugin {}
+    export interface __IAttribute extends ISwaggerSchemaCommonPlugin {}
+  }
+
+  export namespace IGeminiSchema {
+    export interface IInteger extends ISwaggerSchemaPaymentPlugin.INumeric {}
+    export interface INumber extends ISwaggerSchemaPaymentPlugin.INumeric {}
+    export interface IString
+      extends ISwaggerSchemaPaymentPlugin.IString,
+        ISwaggerSchemaSecurityPlugin {}
+    export interface __IAttribute extends ISwaggerSchemaCommonPlugin {}
+  }
+
+  export namespace ILlamaSchema {
+    export interface IInteger extends ISwaggerSchemaPaymentPlugin.INumeric {}
+    export interface INumber extends ISwaggerSchemaPaymentPlugin.INumeric {}
+    export interface IString
+      extends ISwaggerSchemaPaymentPlugin.IString,
+        ISwaggerSchemaSecurityPlugin {}
+    export interface __IAttribute extends ISwaggerSchemaCommonPlugin {}
+  }
+
+  export namespace ILlmSchemaV3 {
+    export interface IInteger extends ISwaggerSchemaPaymentPlugin.INumeric {}
+    export interface INumber extends ISwaggerSchemaPaymentPlugin.INumeric {}
+    export interface IString
+      extends ISwaggerSchemaPaymentPlugin.IString,
+        ISwaggerSchemaSecurityPlugin {}
+    export interface __IAttribute extends ISwaggerSchemaCommonPlugin {}
+  }
+
+  export namespace ILlmSchemaV3_1 {
+    export interface IInteger extends ISwaggerSchemaPaymentPlugin.INumeric {}
+    export interface INumber extends ISwaggerSchemaPaymentPlugin.INumeric {}
+    export interface IString
+      extends ISwaggerSchemaPaymentPlugin.IString,
+        ISwaggerSchemaSecurityPlugin {}
+    export interface __IAttribute extends ISwaggerSchemaCommonPlugin {}
+  }
 }
